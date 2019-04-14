ce57c81a64533ae16c958b377a5e94e4d7b832e9
Thomas Delort <thomas.delort@gmail.com>
2019-04-14 22:39:33 +0200

    fonctionnalité d'ordre des tableaux via titres et taille

--- src/components/compAlbums.vue
+++ src/components/compAlbums.vue
@@ -4,7 +4,7 @@
         <table>
             <tr class="lists__head">
                 <th class="lists__del"></th>
-                <th class="lists__title"><span>Titres</span></th>
+                <th class="lists__title"><span v-on:click="orderedName()">Titres</span></th>
                 <th class="lists__size"></th>
             </tr>
             <tr v-for="item in ALBUMS" :key="item.id" class="lists__element">
@@ -20,13 +20,41 @@
 <script>
   import { ALBUMS } from '../data.js'
 
+  // variables et fonctions pour le tri des listes
+
+let order = "nameDESC";
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
 export default {
   name: 'compAlbums',
   data () {
     return {
       ALBUMS
     }
-  }
+  },
+    methods: {
+        orderedName: function () {
+            if(order === "nameDESC"){
+                ALBUMS.sort(dynamicSort("-name"))
+                order = "nameASC"
+            } 
+            else {
+                ALBUMS.sort(dynamicSort("name"))
+                order = "nameDESC"
+            }
+        }
+    }
 }
 </script>
 
--- src/components/compPlaylists.vue
+++ src/components/compPlaylists.vue
@@ -4,8 +4,8 @@
         <table>
             <tr class="lists__head">
                 <th class="lists__del"></th>
-                <th class="lists__title"><span>Titres</span></th>
-                <th class="lists__size"><span>Taille</span></th>
+                <th class="lists__title"><span v-on:click="orderedName()">Titres</span></th>
+                <th class="lists__size"><span v-on:click="orderedSize()">Taille</span></th>
             </tr>
             <tr v-for="item in PLAYLISTS" :key="item.id" class="lists__element">
                 <td class="lists__del">{{ item.size === 0 ? '&#10680;' : '' }}</td>
@@ -20,12 +20,50 @@
 <script>
   import { PLAYLISTS } from '../data.js'
 
+// variables et fonctions pour le tri des listes
+
+let order = "nameDESC";
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
 export default {
   name: 'compPlaylists',
   data () {
     return {
       PLAYLISTS
     }
-  }
+  },
+    methods: {
+        orderedSize: function () {
+            if(order === "sizeDESC"){
+                PLAYLISTS.sort(dynamicSort("-size"))
+                order = "sizeASC"
+            } 
+            else {
+                PLAYLISTS.sort(dynamicSort("size"))
+                order = "sizeDESC"
+            }
+        },
+        orderedName: function () {
+            if(order === "nameDESC"){
+                PLAYLISTS.sort(dynamicSort("-name"))
+                order = "nameASC"
+            } 
+            else {
+                PLAYLISTS.sort(dynamicSort("name"))
+                order = "nameDESC"
+            }
+        }
+    }
 }
 </script>
